611105ec9637b11dd161497e2555254df9e149df
Tridev Verma <tv072000@gmail.com>
2023-04-24 22:39:57 +0530

    Add Set alarm functionality

--- script.js
+++ script.js
@@ -14,6 +14,7 @@
     "nov",
     "dec",
   ];
+  let NO_OF_ALARMS = 0;
 
   // STOP-WATCH FUNCTIONALITY
   class stopWatch {
@@ -46,12 +47,10 @@
       this.minutes = 0;
       this.seconds = 0;
       this.miliseconds = 0;
-      this.hourMinSecDisplay.innerText = `${convertToTwoDigits(
-        this.hours
-      )}:${convertToTwoDigits(this.minutes)}:${convertToTwoDigits(
-        this.seconds
-      )}`;
-      this.miliSecDisplay.innerText = `${convertToTwoDigits(this.miliseconds)}`;
+      this.hourMinSecDisplay.innerText = `${addZero(this.hours)}:${addZero(
+        this.minutes
+      )}:${addZero(this.seconds)}`;
+      this.miliSecDisplay.innerText = `${addZero(this.miliseconds)}`;
     }
 
     displayTime = () => {
@@ -72,12 +71,10 @@
         }
       }
 
-      this.hourMinSecDisplay.innerText = `${convertToTwoDigits(
-        this.hours
-      )}:${convertToTwoDigits(this.minutes)}:${convertToTwoDigits(
-        this.seconds
-      )}`;
-      this.miliSecDisplay.innerText = `${convertToTwoDigits(this.miliseconds)}`;
+      this.hourMinSecDisplay.innerText = `${addZero(this.hours)}:${addZero(
+        this.minutes
+      )}:${addZero(this.seconds)}`;
+      this.miliSecDisplay.innerText = `${addZero(this.miliseconds)}`;
     };
   }
 
@@ -106,6 +103,7 @@
 
     const newStopWatch = new stopWatch(hourMinSec, miliSec);
 
+    // LISTENERS FOR STOP-WATCH
     startBtn.addEventListener("click", function () {
       newStopWatch.start();
       pauseBtn.addEventListener("click", function () {
@@ -115,29 +113,37 @@
         newStopWatch.stop();
       });
     });
+
+    // PRESET ALARM FORM WITH CURRENT DATE AND TIME
+    const setAlarmBtn = document.querySelector(".set-alarm");
+    setAlarmBtn.addEventListener("click", presetAlarmFields);
+
+    // LISTENER TO LISTEN FORM SUBMIT EVENT TO CREATE NEW ALARM
+    const alarmForm = document.querySelector("#alarm-form");
+    alarmForm.addEventListener("submit", (e) => setNewAlarm(e));
   })();
 
   // GET CURRENT TIME AND FORMAT IT
   function getCurrentTime(currentTime) {
-    return `${convertToTwoDigits(
-      currentTime.getHours() % 12
-    )}:${convertToTwoDigits(currentTime.getMinutes())}:${convertToTwoDigits(
-      currentTime.getSeconds()
-    )} ${currentTime.getHours() >= 12 ? "PM" : "AM"}`;
+    return `${addZero(currentTime.getHours() % 12)}:${addZero(
+      currentTime.getMinutes()
+    )}:${addZero(currentTime.getSeconds())} ${
+      currentTime.getHours() >= 12 ? "PM" : "AM"
+    }`;
   }
 
   // GET CURRENT DATE AND FORMAT IT
   function getCurrentDate(currentDate) {
     const day = daysList[currentDate.getDay()];
     const month = monthsList[currentDate.getMonth()];
-    const date = convertToTwoDigits(currentDate.getDate());
+    const date = addZero(currentDate.getDate());
     const year = currentDate.getFullYear();
 
     return `${day} ${month} ${date} ${year}`;
   }
 
   // CONVERT SINGLE DIGIT NUBMER TO TWO DIGIT STRING
-  function convertToTwoDigits(num) {
+  function addZero(num) {
     return `0${num}`.slice(-2);
   }
 
@@ -172,5 +178,82 @@
     let datetime = new Date();
     time.innerHTML = getCurrentTime(datetime);
     date.innerHTML = getCurrentDate(datetime);
+  }
+
+  // PRESET ALARM FIELDS
+  function presetAlarmFields() {
+    const hourField = document.getElementById("hour");
+    const minuteField = document.getElementById("minute");
+    const secondField = document.getElementById("second");
+    const dateField = document.getElementById("select-date");
+    const meridiemField = document.getElementById("meridiem");
+
+    let datetime = new Date();
+
+    hourField.value = addZero(datetime.getHours() % 12);
+    minuteField.value = addZero(datetime.getMinutes());
+    secondField.value = addZero(datetime.getSeconds());
+    meridiemField.value = datetime.getHours() >= 12 ? "PM" : "AM";
+
+    dateField.valueAsDate = datetime;
+  }
+
+  function setNewAlarm(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const title = formData.get("title");
+    const hour = formData.get("hour");
+    const minute = formData.get("minute");
+    const second = formData.get("second");
+    const meridiem = formData.get("meridiem");
+
+    const dateString = formData.get("select-date");
+    const year = dateString.substring(0, 4);
+    const month = dateString.substring(5, 7);
+    const date = dateString.substring(8);
+
+    const li = document.createElement("li");
+
+    const alarmTime = `${monthsList[month - 1]} ${date} ${year} ${addZero(
+      hour
+    )}:${addZero(minute)}:${addZero(second)} ${meridiem}`;
+
+    li.innerHTML = `<span>${title} - (${alarmTime})</span><a href="#" class="delete-alarm">
+    <span><i class="fa-solid fa-trash"></i></span>
+  </a>`;
+
+    const alarmsList = document.querySelector(".active-alarms > ul");
+
+    alarmsList.prepend(li);
+    NO_OF_ALARMS += 1;
+
+    const newAlarmId = setInterval(() => {
+      const datetime = new Date();
+      let currentTime =
+        getCurrentDate(datetime) + " " + getCurrentTime(datetime);
+      currentTime = currentTime.substring(4);
+      if (alarmTime == currentTime) {
+        alert(title);
+        clearInterval(newAlarmId);
+        NO_OF_ALARMS -= 1;
+        alarmsList.removeChild(li);
+      }
+      // console.log(alarmTime);
+      // console.log(currentTime);
+    }, 1000);
+
+    // DELETE ALARM LISTENER
+    deleteAlarm(li, alarmsList, newAlarmId);
+  }
+
+  // DELETE ALARM FROM DOM AND CLEAR INTERAL FOR RUNNING ALARM INSTANCE
+  function deleteAlarm(li, alarmsList, alarmId) {
+    const deleteLink = li.querySelector(".delete-alarm");
+    deleteLink.addEventListener("click", function (e) {
+      e.preventDefault();
+      alarmsList.removeChild(li);
+      clearInterval(alarmId);
+    });
   }
 }
